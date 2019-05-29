13041c241f1028b13658e04b10ea4056694fb77a
Lucas Dimer Justo <37344488+LucasJusto@users.noreply.github.com>
2019-05-29 03:40:40 -0300

    ajustes de compatibilidade com o novo grafo

--- T2-Alest-II/Main.java
+++ T2-Alest-II/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args)throws FileNotFoundException{
 		Scanner usr = new Scanner(System.in);
@@ -10,11 +11,12 @@ public class Main {
 		Lista todosCastelos = grafo.getCastelos();
 		Castelo fragolleto = todosCastelos.get(0);
 		long initialTime = System.currentTimeMillis(); //comeco do algoritmo
-		grafo.calculaMaxCastelos(fragolleto);
+		ArrayList<Integer> caminhoAtual = new ArrayList<Integer>();
+		caminhoAtual.add(0);
+		grafo.calculaMaxCastelos(fragolleto,caminhoAtual);
 		int maxCastelos = grafo.getMaxCastelos();
-		int jeitos = grafo.getJeitos();
 		long finalTime = System.currentTimeMillis();
 		long duration = finalTime-initialTime;
-		System.out.println("Caso: " +arq.substring(6,arq.length())+"   Resposta: "+maxCastelos+"   Tempo: "+duration+"ms");
+		System.out.println("Caso: " +arq.substring(6,arq.length())+"   Resposta: "+maxCastelos+"   Caminho: "+grafo.getCaminho()+"   Tempo: "+duration+"ms");
 	}
 }
